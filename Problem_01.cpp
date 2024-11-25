105658debf05ebc8ead3d680d98342dc8c0dbdf9
Md Nazmul Hasan <nazmul7762@gmail.com>
2024-11-25 18:50:41 +0600

    Problem 01 updated

--- Problem_01.cpp
+++ Problem_01.cpp
@@ -1,55 +1,46 @@
-//Problem 01
-//     Write a C/C++ program tha read the following string: "Md. Tareq Zaman, Part-3, 2011"
-// 	    a) Count number of words, letters, digits and other characteristics
-// 	    b) Separates letters, digits and others characters.
-
-
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
+using namespace std;
 
 int main() {
     char str[] = "Md. Tareq Zaman, Part-3, 2011";
-    int i, words = 0, letters = 0, digits = 0, others = 0;
+    string letter, digit, other;
+    int i = 0, words = 0, letters = 0, digits = 0, others = 0;
 
-    for (i = 0; str[i] != '\0'; i++) {
+    // Count number of words, letters, digits, and other characters
+    while (str[i] != '\0') {
         if (isalpha(str[i])) {
             letters++;
+            letter.push_back(str[i]);
         } else if (isdigit(str[i])) {
             digits++;
-        } else if (str[i] == ' ' || str[i] == ',' || str[i] == '-') {
+            digit.push_back(str[i]);
+        } else if (str[i] == ' ') {
             words++;
         } else {
             others++;
+            other.push_back(str[i]);
         }
+        i++;
     }
+    words++; // Increment words for the last word
 
-    // Last word doesn't have space after it
-    words++;
-
-    printf("Number of words: %d\n", words);
-    printf("Number of letters: %d\n", letters);
-    printf("Number of digits: %d\n", digits);
-    printf("Number of other characters: %d\n", others);
+    cout << "Number of words: " << words << endl;
+    cout << "Number of letters: " << letters << endl;
+    cout << "Number of digits: " << digits << endl;
+    cout << "Number of other characters: " << others << endl;
 
     // Separating letters, digits, and other characters
-    printf("\nLetters: ");
-    for (i = 0; str[i] != '\0'; i++) {
-        if (isalpha(str[i])) {
-            printf("%c ", str[i]);
-        }
+    cout << "Letters: ";
+    for (char c : letter) {
+        cout << c << " ";
     }
-
-    printf("\nDigits: ");
-    for (i = 0; str[i] != '\0'; i++) {
-        if (isdigit(str[i])) {
-            printf("%c ", str[i]);
-        }
+    cout << "\nDigits: ";
+    for (char c : digit) {
+        cout << c << " ";
     }
-
-    printf("\nOther characters: ");
-    for (i = 0; str[i] != '\0'; i++) {
-        if (!isalpha(str[i]) && !isdigit(str[i]) && str[i] != ' ' && str[i] != ',' && str[i] != '-') {
-            printf("%c ", str[i]);
-        }
+    cout << "\nOther characters: ";
+    for (char c : other) {
+        cout << c << " ";
     }
 
     return 0;
